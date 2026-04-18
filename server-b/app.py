941f711a48b4b3ba75db343da0b47760a1f40098
Harish1604 <harish16042005@gmail.com>
2026-04-18 22:19:41 +0530

    feat/graphana-prometheus

--- server-b/app.py
+++ server-b/app.py
@@ -1,10 +1,15 @@
 from fastapi import FastAPI
+from prometheus_client import Counter, generate_latest
+from fastapi.responses import Response
 import socket
 
 app = FastAPI()
 
+REQUESTS = Counter("app_requests_total", "Total Requests")
+
 @app.get("/")
 def home():
+    REQUESTS.inc()
     return {
         "server": "B",
         "host": socket.gethostname(),
@@ -14,3 +19,7 @@ def home():
 @app.get("/health")
 def health():
     return {"status": "ok"}
+
+@app.get("/metrics")
+def metrics():
+    return Response(generate_latest(), media_type="text/plain")
